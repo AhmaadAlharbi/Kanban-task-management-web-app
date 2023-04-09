d7d8d64baa54e044e801b20f44f653229cd35762
AhmadAlharbi <ahmaadzaid7@gmail.com>
2023-04-09 14:30:56 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Navbar from "@/components/Navbar.vue";
+import Sidebar from "@/components/Sidebar.vue";
+</script>
+
+<template>
+  <Navbar />
+  <Sidebar />
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mt-10 ml-5 bg-white shadow-xl w- py-7 w-80">
+    <div>
+      <img src="@/assets/images/logo-dark.svg" alt="" />
+      <div class="mt-10">
+        <p class="uppercase text-gray-500 my-5">all boards</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
