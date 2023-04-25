6edbda7d5edca875466f910b617b6aad10342f13
ahmadZaid <ahmaadzaid7@gmail.com>
2023-04-25 05:17:15 +0300

    add comments to home.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@
         </li>
       </ul>
     </nav>
-    <div>
+    <!-- <div>
       <div v-if="documents.length > 0">
         <h1 class="bg-red-200">Documents</h1>
         {{ documents }}
@@ -31,25 +31,40 @@
 
       <div v-else-if="error">{{ error }}</div>
       <div v-else>Loading...</div>
-    </div>
+    </div> -->
     <!-- todo -->
     <div v-if="columns.length > 0">
-      <div class="flex justify-around">
+      <!-- Loop through the columns -->
+      <div class="flex justify-around mt-3">
         <div class="flex flex-col" v-for="col in columns" :key="col.id">
+          <!-- Display the column name and id -->
           <h1 class="bg-gray-300 px-2">{{ col.name }} - {{ col.id }}</h1>
           <div v-if="cards">
-            <div
-              class="flex flex-col shadow-md mt-3 py-3 px-6 bg-white w-80 mb-8"
-              v-for="card in cards"
-              :key="card.id"
-            >
-              <div v-if="card.column_id == col.id">
-                <p class="font-bold" v-if="card">{{ card.title }}</p>
-                <!-- subtasks -->
-                <div v-for="sub in subtasks" :key="sub.id">
-                  <p class="text-gray-500" v-if="card.id === sub.card_id">
-                    {{ completedSubtasks.length }} - of {{ subtasks.length }}
-                  </p>
+            <!-- Loop through the cards -->
+            <div v-for="card in cards" :key="card.id">
+              <!-- Only display the card if it belongs to the current column -->
+              <div v-if="card.column_id === col.id">
+                <div
+                  class="flex flex-col shadow-md mt-3 py-3 px-6 bg-white w-80 mb-8"
+                >
+                  <!-- Display the card title -->
+                  <p class="font-bold" v-if="card">{{ card.title }}</p>
+                  <!-- Display subtask information if there are any subtasks for the card -->
+                  <div v-if="subtasks.some((sub) => sub.card_id === card.id)">
+                    <p class="text-gray-500">
+                      {{
+                        completedSubtasks.filter(
+                          (sub) => sub.card_id === card.id
+                        ).length
+                      }}
+                      of
+                      {{
+                        subtasks.filter((sub) => sub.card_id === card.id).length
+                      }}
+                      subtasks
+                    </p>
+                  </div>
+                  <!-- Display "No subtasks" message if there are no subtasks for the card -->
                   <p class="text-gray-500" v-else>No subtasks</p>
                 </div>
               </div>
